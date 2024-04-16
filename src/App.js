6752211a0dc662c9c6e5b35d56586f012b9cb70b
Leandro <ufmt.leandro@gmail.com>
2024-04-16 13:03:40 -0400

    Primeiro commit das aulas do Alura, criando projeto react livros books

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import Logo from './componentes/Logo'
+import perfil from './imagens/perfil.svg'
+import sacola from './imagens/sacola.svg'
+
+const textoOpcoes = ['CATEGORIAS', 'FAVORITOS', 'MINHA ESTANTE']
+const icones = [perfil, sacola]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <Logo></Logo>
+        <ul className='opcoes'>
+          {textoOpcoes.map((texto) => (
+            <li className='opcao'><p>{texto}</p></li>
+          ))}
+        </ul>
+        <ul className='icones'>
+          {icones.map((icon) => (
+            <li className='opcao'><img src={icon}></img></li>
+          ))}
+        </ul>
       </header>
     </div>
   );
