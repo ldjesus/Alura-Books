76ebe7f9e143bd83610a5fc479dfc80bc5dfb59f
Leandro <ufmt.leandro@gmail.com>
2024-04-17 19:46:42 -0400

    Estilizando o botão de pesquisa

---
+++ src/componentes/Pesquisa/index.js
@@ -0,0 +1,13 @@
+import Input from '../Input'
+
+function Pesquisa(){
+    return(
+        <section>
+            <h2>Já sabe por onde começar?</h2>
+            <h3>Encontre seu livro em nossa instante.</h3>
+            <Input placeholder="Escreva sua próxima leitura" />
+        </section>
+    )
+}
+
+export default Pesquisa
