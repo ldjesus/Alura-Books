4a3d766acaf13ba2c32ce84b2b56661d06be9bf3
Leandro <ufmt.leandro@gmail.com>
2024-04-17 15:40:07 -0400

    Eliminando os arquivos css de header, App e incluindo o styled como container

--- src/componentes/Header/index.js
+++ src/componentes/Header/index.js
@@ -1,15 +1,21 @@
-import './estilo.css'
 import Logo from '../Logo'
 import OpcoesHeader from '../OpcoesHeader'
 import IconesHeader from '../IconesHeader'
+import styled from 'styled-components'
+
+const HeaderContainer = styled.header`
+    background-color: #fff;
+    display: flex;
+    justify-content: center;
+`
 
 function Header(){
     return(
-        <header className='App-header'>
-        <Logo></Logo>
-        <OpcoesHeader></OpcoesHeader>
-        <IconesHeader></IconesHeader>
-        </header> 
+        <HeaderContainer>
+            <Logo></Logo>
+            <OpcoesHeader></OpcoesHeader>
+            <IconesHeader></IconesHeader>
+        </HeaderContainer> 
     )
 }
 
