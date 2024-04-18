5f57135348c80536e77898c32c7efa0fd7ad029f
Leandro <ufmt.leandro@gmail.com>
2024-04-17 20:04:03 -0400

    layout novo

--- src/componentes/Pesquisa/index.js
+++ src/componentes/Pesquisa/index.js
@@ -1,12 +1,34 @@
 import Input from '../Input'
+import styled from 'styled-components'
+
+const PesquisaContainer = styled.section`
+    background-image: linear-gradient(90deg,#002F52 35%, #326589)
+    color: #FFF;
+    text-align:center;
+    padding:85px 0;
+    height: 270px;
+    width: 100%;
+`
+const Titulo = styled.h2`
+    color: #FFF;
+    font-size: 36px;
+    text-align:center;
+    width:100%;
+`
+
+const Subtitulo = styled.h3`
+    font-size: 16px;
+    font-weight: 500;
+    margin-bottom: 40px;
+`
 
 function Pesquisa(){
     return(
-        <section>
-            <h2>Já sabe por onde começar?</h2>
-            <h3>Encontre seu livro em nossa instante.</h3>
+        <PesquisaContainer>
+            <Titulo>Já sabe por onde começar?</Titulo>
+            <Subtitulo>Encontre seu livro em nossa instante.</Subtitulo>
             <Input placeholder="Escreva sua próxima leitura" />
-        </section>
+        </PesquisaContainer>
     )
 }
 
